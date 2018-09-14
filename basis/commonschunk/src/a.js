7c765442f1b995245314158ee6a642adad376cda
baihe6 <baihe3@jd.com>
2018-09-14 20:06:27 +0800

    gh-pages

--- basis/commonschunk/src/a.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	sayHello: function() {
-		console.log('hello a');
-	}
-}
--- vue-demo/build/prod.js
+++
@@ -1,6 +0,0 @@
-var webpack = require('webpack');
-var config = require('./webpack.prod.conf');
-
-webpack(config, function (err, stats) {
-  console.log(stats.toString({chunks: false, color: true}));
-});
