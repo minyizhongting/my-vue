4bee730ee4eadedbe9f01de37060eb39f9c2023c
baihe6 <baihe3@jd.com>
2018-09-21 19:40:07 +0800

    clean

--- vue-example-cli3/src/mock.js
+++
@@ -1,8 +0,0 @@
-const Mock = require('mockjs');
-
-// Mock.mock(url, post/get, 返回的数据);
-Mock.mock('/api/data', 'get', (req, res) => {   // 请求被mockjs拦截，不会出现在network面板中
-  return {
-    data: ['a', 'b']
-  }
-});
